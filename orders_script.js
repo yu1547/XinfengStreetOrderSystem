0c8b07cc06a3560fd4791c4625da9b5fb1423874
yu1547 <ppeennnnyy1547@gmail.com>
2024-10-21 04:25:26 +0800

    add orders
    點菜單

---
+++ orders_script.js
@@ -0,0 +1,21 @@
+// 處理數量增加和減少功能
+function increaseQuantity(id) {
+    var quantityInput = document.getElementById(id);
+    var currentValue = parseInt(quantityInput.value);
+    quantityInput.value = currentValue + 1;
+}
+
+function decreaseQuantity(id) {
+    var quantityInput = document.getElementById(id);
+    var currentValue = parseInt(quantityInput.value);
+    if (currentValue > 1) {
+        quantityInput.value = currentValue - 1;
+    }
+}
+
+// 使用 jQuery UI Datepicker 來選擇日期
+$(document).ready(function() {
+    $("#pickup-date").datepicker({
+        dateFormat: "yy-mm-dd" // 設置日期格式
+    });
+});
