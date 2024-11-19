e56d9c3f92993b5c21d654e6cfc5a60e2efe9518
YunChen31 <luosammy11@gmail.com>
2024-11-20 00:41:12 +0800

    菜單瀏覽、刪除餐點、購物車session(只有後端連結資料庫)

--- XinfengStreetOrderSystem/src/main/java/ntou/cs/XinfengStreetOrderSystem/repository/MenuItemRepository.java
+++ XinfengStreetOrderSystem/src/main/java/ntou/cs/XinfengStreetOrderSystem/repository/MenuItemRepository.java
@@ -1,8 +1,12 @@
 package ntou.cs.XinfengStreetOrderSystem.repository;
 
+import ntou.cs.XinfengStreetOrderSystem.entity.MenuItem;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-import ntou.cs.XinfengStreetOrderSystem.entity.MenuItem;
+import java.util.List;
 
 public interface MenuItemRepository extends MongoRepository<MenuItem, String> {
+    List<MenuItem> findByCategory(String category);
+    List<MenuItem> findByNameContainingIgnoreCase(String query);
 }
+
