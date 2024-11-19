7251a41c6a5c50bfb275fd0358188d801a17c258
yu1547 <ppeennnnyy1547@gmail.com>
2024-11-20 00:28:17 +0800

    刪掉一些多餘的

--- XinfengStreetOrderSystem/src/main/java/ntou/cs/XinfengStreetOrderSystem/repository/MenuItemRepository.java
+++ XinfengStreetOrderSystem/src/main/java/ntou/cs/XinfengStreetOrderSystem/repository/MenuItemRepository.java
@@ -1,12 +1,8 @@
 package ntou.cs.XinfengStreetOrderSystem.repository;
 
-import java.util.List;
-
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-import ntou.cs.XinfengStreetOrderSystem.model.MenuItem;
+import ntou.cs.XinfengStreetOrderSystem.entity.MenuItem;
 
 public interface MenuItemRepository extends MongoRepository<MenuItem, String> {
-    // 查詢菜單名稱的方法（如果需要的話）
-    List<MenuItem> findByName(String name);
 }
